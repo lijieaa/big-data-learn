230b6f9ee352480d8a066e74bcb86d523ebc8f64
lijieaa <952054767@qq.com>
2017-10-11 21:45:31 +0800

    open

--- hdfs/src/main/java/com/visizen/lijie/hdfs/HdfsApp.java
+++ hdfs/src/main/java/com/visizen/lijie/hdfs/HdfsApp.java
@@ -1,20 +1,36 @@
 package com.visizen.lijie.hdfs;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+
 /**
- * Created by Administrator on 2017/10/11.
+ * hdfs测试
  */
 public class HdfsApp {
 
+    private FileSystem fs;
+
     @Before
-    public void init(){
-        System.out.println("init");
+    public void init() throws IOException {
+        Configuration configuration=new Configuration();
+        fs=FileSystem.get(configuration);
     }
 
+    /**
+     * 读取hdfs文件系统上文件的内容
+     * @throws IOException
+     */
     @Test
-    public void get(){
-        System.out.println("get");
+    public void open() throws IOException {
+        FSDataInputStream fsDataInputStream = fs.open(new Path("/a.txt"));
+        IOUtils.copyBytes(fsDataInputStream,System.out,4096,true);
+        IOUtils.closeStream(fsDataInputStream);
     }
 }
