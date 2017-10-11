8ae8a81abbae074d6b9e0a4e4efe2f3f58f6a55a
lijieaa <952054767@qq.com>
2017-10-11 21:29:25 +0800

    Initial commit

---
+++ hdfs/src/main/java/com/visizen/lijie/hdfs/HdfsApp.java
@@ -0,0 +1,20 @@
+package com.visizen.lijie.hdfs;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2017/10/11.
+ */
+public class HdfsApp {
+
+    @Before
+    public void init(){
+        System.out.println("init");
+    }
+
+    @Test
+    public void get(){
+        System.out.println("get");
+    }
+}
