3bad7ab84fcd7771f60555a5f4e1cf8516415dcb
lijieaa <952054767@qq.com>
2017-10-12 21:48:55 +0800

    open create

--- hdfs/src/main/java/com/visizen/lijie/hdfs/HdfsApp.java
+++ hdfs/src/main/java/com/visizen/lijie/hdfs/HdfsApp.java
@@ -2,13 +2,18 @@ package com.visizen.lijie.hdfs;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FSDataInputStream;
+import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URL;
 
 /**
  * hdfs测试
@@ -19,7 +24,11 @@ public class HdfsApp {
 
     @Before
     public void init() throws IOException {
+
+        System.setProperty("HADOOP_USER_NAME","jay");
+
         Configuration configuration=new Configuration();
+
         fs=FileSystem.get(configuration);
     }
 
@@ -29,8 +38,34 @@ public class HdfsApp {
      */
     @Test
     public void open() throws IOException {
-        FSDataInputStream fsDataInputStream = fs.open(new Path("/a.txt"));
+
+        FSDataInputStream fsDataInputStream = fs.open(new Path("/input"));
+
         IOUtils.copyBytes(fsDataInputStream,System.out,4096,true);
+
         IOUtils.closeStream(fsDataInputStream);
     }
+
+    /**
+     * 上传文件
+     * @throws IOException
+     */
+
+    @Test
+    public void create() throws IOException {
+
+        URL resourceUrl = this.getClass().getResource("/word.txt");
+
+        FSDataOutputStream fsDataOutputStream = fs.create(new Path("/word.txt"));
+
+        FileInputStream fileInputStream=new FileInputStream(new File(resourceUrl.getPath()));
+
+        IOUtils.copyBytes(fileInputStream,fsDataOutputStream,4096,true);
+
+
+       IOUtils.closeStream(fsDataOutputStream);
+
+        IOUtils.closeStream(fileInputStream);
+    }
+
 }
